d399f8dc133bcb122aa2ec6ff447b83891bd63ee
775371 <44507768+775371@users.noreply.github.com>
2019-02-20 23:53:12 -0500

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -126,7 +126,7 @@ CTss(int n, double *y[], double *value[2],  double *con_mean, double *tr_mean,
     
     *tr_mean= temp1 / ttreat;
     *con_mean= temp0 / (twt - ttreat);
-    *value[2] = {&effect, &effects);
+    *value[2] = {&effect, &effects};
   
 
     //*risk = 4 * twt * max_y * max_y - alpha * twt * effect * effect + 
